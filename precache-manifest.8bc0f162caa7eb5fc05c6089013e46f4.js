9cfb499064b62aad995ca9826d5dfaa22d2803ae
Dmitriy <dmitriy.yarmola@gmail.com>
2020-05-12 21:53:47 +0300

    Updates

---
+++ precache-manifest.8bc0f162caa7eb5fc05c6089013e46f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4d6f05b0a74f064394de30c19e2a29",
+    "url": "/Sort-tickets/index.html"
+  },
+  {
+    "revision": "7900039dc8866d2b318e",
+    "url": "/Sort-tickets/static/css/main.cd0f8a2e.chunk.css"
+  },
+  {
+    "revision": "48f3218e1c3dcb51f7e2",
+    "url": "/Sort-tickets/static/js/2.3d39e71e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sort-tickets/static/js/2.3d39e71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7900039dc8866d2b318e",
+    "url": "/Sort-tickets/static/js/main.4aa5fcd9.chunk.js"
+  },
+  {
+    "revision": "717c0c7bcce441efd7b6",
+    "url": "/Sort-tickets/static/js/runtime-main.188d6f47.js"
+  },
+  {
+    "revision": "66a91fa30cce1817246a19625c2d950a",
+    "url": "/Sort-tickets/static/media/logo.66a91fa3.svg"
+  }
+]);
