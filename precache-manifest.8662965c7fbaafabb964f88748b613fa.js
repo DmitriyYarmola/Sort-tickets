9ead9b1ebdcfafb959f2fac6d148ed6e4c9de4c8
Dmitriy <dmitriy.yarmola@gmail.com>
2020-05-13 18:42:03 +0300

    Updates

---
+++ precache-manifest.8662965c7fbaafabb964f88748b613fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca7fea5219f96b45efb7d489d1ab4f6",
+    "url": "/Sort-tickets/index.html"
+  },
+  {
+    "revision": "4a844f759b2af9139bbc",
+    "url": "/Sort-tickets/static/css/main.cd0f8a2e.chunk.css"
+  },
+  {
+    "revision": "48f3218e1c3dcb51f7e2",
+    "url": "/Sort-tickets/static/js/2.3d39e71e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sort-tickets/static/js/2.3d39e71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a844f759b2af9139bbc",
+    "url": "/Sort-tickets/static/js/main.1befa8b1.chunk.js"
+  },
+  {
+    "revision": "717c0c7bcce441efd7b6",
+    "url": "/Sort-tickets/static/js/runtime-main.188d6f47.js"
+  },
+  {
+    "revision": "66a91fa30cce1817246a19625c2d950a",
+    "url": "/Sort-tickets/static/media/logo.66a91fa3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sort-tickets%20/precache-manifest.b3b9f5162460b967d559d7adfb1e3090.js"
+  "/Sort-tickets/precache-manifest.8662965c7fbaafabb964f88748b613fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sort-tickets%20/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sort-tickets/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
